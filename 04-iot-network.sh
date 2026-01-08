1a99c03c22ede177e94a1e072318f227ce1481e7
Kai Evans <hi@kaievans.co>
2026-01-08 20:44:22 +1100

    Adding the IoT network isolation

---
+++ 04-iot-network.sh
@@ -0,0 +1,57 @@
+#! /usr/bin sh
+
+# Create IoT interface with IP 172.20.2.1
+uci set network.iot='interface'
+uci set network.iot.proto='static'
+uci set network.iot.ipaddr='172.20.2.1'
+uci set network.iot.netmask='255.255.255.0'
+
+# Configure DHCP for IoT network
+uci set dhcp.iot='dhcp'
+uci set dhcp.iot.interface='iot'
+uci set dhcp.iot.start='100'
+uci set dhcp.iot.limit='150'
+uci set dhcp.iot.leasetime='12h'
+
+uci commit network
+uci commit dhcp
+
+# Create IoT firewall zone
+uci add firewall zone
+uci set firewall.@zone[-1].name='iot'
+uci set firewall.@zone[-1].input='REJECT'
+uci set firewall.@zone[-1].output='ACCEPT'
+uci set firewall.@zone[-1].forward='REJECT'
+uci set firewall.@zone[-1].network='iot'
+
+# Allow IoT → WAN (Internet access)
+uci add firewall forwarding
+uci set firewall.@forwarding[-1].src='iot'
+uci set firewall.@forwarding[-1].dest='wan'
+
+# Allow LAN → IoT (main network can access IoT devices)
+uci add firewall forwarding
+uci set firewall.@forwarding[-1].src='lan'
+uci set firewall.@forwarding[-1].dest='iot'
+
+# Allow DNS and DHCP from IoT to router
+uci add firewall rule
+uci set firewall.@rule[-1].name='Allow-IoT-DNS'
+uci set firewall.@rule[-1].src='iot'
+uci set firewall.@rule[-1].dest_port='53'
+uci set firewall.@rule[-1].proto='tcp udp'
+uci set firewall.@rule[-1].target='ACCEPT'
+
+uci add firewall rule
+uci set firewall.@rule[-1].name='Allow-IoT-DHCP'
+uci set firewall.@rule[-1].src='iot'
+uci set firewall.@rule[-1].dest_port='67'
+uci set firewall.@rule[-1].proto='udp'
+uci set firewall.@rule[-1].target='ACCEPT'
+
+uci commit firewall
+
+# restart everything
+/etc/init.d/network restart
+/etc/init.d/firewall restart
+wifi
