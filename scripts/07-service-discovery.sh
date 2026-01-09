697d4ad52b3bb9ed4bda24cfc2d0ddc449b5fe6b
Kai Evans <hi@kaievans.co>
2026-01-09 19:39:27 +1100

    Adding IoT -> Lan service discovery

---
+++ scripts/07-service-discovery.sh
@@ -0,0 +1,25 @@
+#! /usr/bin sh
+
+# adds service discovery for the IoT -> Lan
+
+# Install avahi mDNS reflector
+opkg update
+opkg install avahi-daemon avahi-dbus-daemon
+
+# Configure avahi to reflect between lan and iot
+cat > /etc/avahi/avahi-daemon.conf << 'EOF'
+[server]
+use-ipv4=yes
+use-ipv6=no
+enable-dbus=yes
+allow-interfaces=br-lan,br-iot
+deny-interfaces=eth1
+
+[reflector]
+enable-reflector=yes
+reflect-ipv=yes
+EOF
+
+# Enable and start
+/etc/init.d/avahi-daemon enable
+/etc/init.d/avahi-daemon start
