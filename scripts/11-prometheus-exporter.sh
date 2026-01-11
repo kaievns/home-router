64326f0a1171fda408b1f716169e598e5be6b779
Kai Evans <hi@kaievans.co>
2026-01-11 14:10:02 +1100

    Adding prometheus metrics exporter setup

---
+++ scripts/11-prometheus-exporter.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#
+# Setting up the prometheus exporter
+#
+
+opkg update
+opkg install \
+  prometheus-node-exporter-lua \
+  prometheus-node-exporter-lua-openwrt \
+  prometheus-node-exporter-lua-nat_traffic \
+  prometheus-node-exporter-lua-netstat \
+  prometheus-node-exporter-lua-wifi \
+  prometheus-node-exporter-lua-wifi_stations \
+  prometheus-node-exporter-lua-thermal \
+  prometheus-node-exporter-lua-hwmon \
+  prometheus-node-exporter-lua-uci_dhcp_host \
+  prometheus-node-exporter-lua-textfile \
+  curl \
+  bc
+
+# Enable and start
+/etc/init.d/prometheus-node-exporter-lua enable
+/etc/init.d/prometheus-node-exporter-lua start
+
+# Verify
+# curl http://127.0.0.1:9100/metrics | grep node_
